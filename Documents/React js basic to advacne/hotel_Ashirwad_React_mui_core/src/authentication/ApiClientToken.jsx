f49d0b3e7e00e3a2102f7521c6b2990231e6bbc2
Aakash Amit Bakhle <tusharphalke1995@gmail.com>
2024-10-03 21:10:27 +0530

    chnage

--- Documents/React js basic to advacne/hotel_Ashirwad_React_mui_core/src/authentication/ApiClientToken.jsx
+++ Documents/React js basic to advacne/hotel_Ashirwad_React_mui_core/src/authentication/ApiClientToken.jsx
@@ -5,9 +5,8 @@ const axiosInstanceToken = axios.create({
     baseURL: "http://localhost:8976/",
   timeout: 5000,
   headers: {
-    'Content-Type': 'application/json',
-    'Content-Security-Policy': "script-src 'self' 'unsafe-eval'",
-    "Content-Type": "multipart/form-data",
+    'Content-Type': 'multipart/form-data',
+  'Content-Security-Policy': "script-src 'self' 'unsafe-eval'"
   },
 });
 
